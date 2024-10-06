9195a7b708292754cc179f5553a66527477373c9
Ishita Paul <119348118+ishita-5062@users.noreply.github.com>
2024-10-06 23:06:14 +0530

    slight css changes

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ const Storyline = () => {
         <h1 className={styles.title}>GENESIS</h1>
         <p className={styles.paragraph}>
           Celestial Biscuit contains ancient and rare knowledge—only accessible by those worthy of the club's secrets. 
-          The Guardians have set complex password puzzles to protect this knowledge. You’ve been chosen to face the trials and crack the password, 
+          The Guardians have set complex password puzzles to protect this knowledge. You&apos;ve been chosen to face the trials and crack the password, 
           gaining access to the wisdom of the celestial founders, the key to being in the top 1%.
         </p>
         <p className={styles.paragraph}>
