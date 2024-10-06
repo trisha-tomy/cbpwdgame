586bd44b79c6ad0a888f87b1f3d024e9c1cc3fb7
Ishita Paul <119348118+ishita-5062@users.noreply.github.com>
2024-10-06 23:08:25 +0530

    apostrophe error

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ const Storyline = () => {
       <div className={styles.storyContent}>
         <h1 className={styles.title}>GENESIS</h1>
         <p className={styles.paragraph}>
-          Celestial Biscuit contains ancient and rare knowledge—only accessible by those worthy of the club's secrets. 
+          Celestial Biscuit contains ancient and rare knowledge—only accessible by those worthy of the club&apos;s secrets. 
           The Guardians have set complex password puzzles to protect this knowledge. You&apos;ve been chosen to face the trials and crack the password, 
           gaining access to the wisdom of the celestial founders, the key to being in the top 1%.
         </p>
